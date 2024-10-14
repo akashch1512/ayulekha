f5839e5e75212f9cfbe983496a024761c3029389
akash <akashchaudhari1012@gmail.com>
2024-10-14 19:55:20 +0530

    taiwind

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // Look for class names in the src folder
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
